33e758a2b89c5f43f01ad4d6bd57d69ad65b5cdc
luizrozao <luiz.barati@edu.senai.br>
2026-08-18 13:44:57 -0300

    Update 1_variaveis.php

--- inicio-revisao/1_variaveis.php
+++ inicio-revisao/1_variaveis.php
@@ -1,1 +1,18 @@
-<!-- 1ª Digitação (Aqui) -->
+<?php
+
+$preco = 50;
+$quantidade = 5;
+
+$total = $preco * $quantidade;
+
+if ($total >= 200) {
+    $desconto = $total * 0.10;
+    $totalFinal = $total - $desconto;
+
+    echo "Valor total: R$ " . $total . "<br>";
+    echo "Desconto de 10%: R$ " . $desconto . "<br>";
+    echo "Valor final: R$ " . $totalFinal;
+} else {
+    echo "Valor total: R$ " . $total . "<br>";
+    echo "Sem desconto.";
+}
